05deadc23d759e8eefefe5ed4d6d45954dc32ea6
Jesús <jtipian@gmail.com>
2025-10-28 11:54:46 +0100

    add service worker lady.exe

--- GameLab/lady_exe/service-worker.js
+++ GameLab/lady_exe/service-worker.js
@@ -1,3 +1,7 @@
-self.addEventListener("fetch", () => { });
+self.addEventListener("install", (event) => {
+    console.log("Service Worker instalado");
+});
 
-
+self.addEventListener("fetch", (event) => {
+    // Este SW no hace nada especial, solo permite la instalación
+});
